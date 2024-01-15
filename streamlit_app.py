edeaac9df0334033eeb576007e992a88b5b9abbc
AbeerDas <67808617+AbeerDas@users.noreply.github.com>
2024-01-15 15:13:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -365,8 +365,8 @@ Email: abeerdas647@gmail.com
 
 st.markdown(f"""
 <h3>Additional Resources</h3>
-Another project I looked at : [Stock Price Prediction Using Machine Learning Project]({"https://www.projectpro.io/article/stock-price-prediction-using-machine-learning-project/571"})
-<br>Highly reccomended playlist for beginners: [StatQuest]({"https://www.youtube.com/watch?v=zxagGtF9MeU&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&ab_channel=StatQuestwithJoshStarmer"})
+Another project I looked at : [Stock Price Prediction Using Machine Learning Project]("https://www.projectpro.io/article/stock-price-prediction-using-machine-learning-project/571")
+<br>Highly reccomended playlist for beginners: [StatQuest]("https://www.youtube.com/watch?v=zxagGtF9MeU&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&ab_channel=StatQuestwithJoshStarmer")
 """, unsafe_allow_html=True)
 
 import os
